de6aeb79c045740f11d99a59ef2c7786d852a57b
Rodrigo Muñoz Muñoz <rodrigo.mcuadrada@gmail.com>
2017-08-16 03:51:50 -0500

    updates

---
+++ ILoLSC.py
@@ -0,0 +1,18 @@
+"""Itspaper´s League of Legends Stat Calculator"""
+
+# Module Imports
+
+import IEM as iem
+import pickle
+
+# MetaData
+
+__version__ = "Alpha"
+__author__ = "Rodrigo 'ItsPaper' Muñoz"
+__email__ = "rodrigo.mcuadrada@gmail.com"
+
+# Variable Declarations
+
+# Functions
+
+# Main Code
